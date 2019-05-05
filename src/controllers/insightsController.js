57d1fa8b1954392cdc4939950ac3f145eb25c36f
Nikhil Vijayan <nkhilv@gmail.com>
2019-05-05 21:04:31 +0100

    Add insights controller

---
+++ src/controllers/insightsController.js
@@ -0,0 +1,37 @@
+const Request = require("request");
+const {
+  createUniqueCategories,
+  createObject,
+  calculateAverageValue,
+  calculateTotalNumber,
+  calculateTotalValue,
+  groupCategoryItems,
+  createCategoryObject
+} = require("../helpers/helpers");
+
+let data;
+
+exports.categories = async (req, res) => {
+  const endpoint = "https://transactions.spokedev.xyz/transactions";
+  Request.get(endpoint, async (error, response, body) => {
+    if (error) {
+      return console.dir(error);
+    }
+    data = JSON.parse(body);
+  });
+  const categoryNames = await createUniqueCategories(data || []);
+  const arrayOfObjects = categoryNames.map(category =>
+    this.calculateCategories(data, category)
+  );
+  const processedData = createCategoryObject(categoryNames, arrayOfObjects);
+  res.json(processedData);
+};
+
+exports.calculateCategories = (data, categoryName) => {
+  const categoryItems = groupCategoryItems(data, categoryName);
+  const totalValue = calculateTotalValue(categoryItems);
+  const totalNumber = calculateTotalNumber(categoryItems);
+  const averageValue = calculateAverageValue(totalValue, totalNumber);
+  const categoryObject = createObject(totalNumber, totalValue, averageValue);
+  return categoryObject;
+};

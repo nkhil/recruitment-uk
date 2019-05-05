c38cc1d1a0191e7ce3cc79addfe1810533126432
Nikhil Vijayan <nkhilv@gmail.com>
2019-05-05 21:04:52 +0100

    Use insightsController in insightsRoutes

--- src/routers/insights.js
+++ src/routers/insights.js
@@ -1,25 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { catchErrors } = require("../helpers/helpers");
+const insightsController = require("../controllers/insightsController");
 
-router.get('/categories', async (req, res, next) => {
+router.get("/categories", catchErrors(insightsController.categories));
+
+router.get("/cashflow", async (req, res, next) => {
   try {
-    res.status(501).json({ message: 'Not Implemented' });
+    res.status(501).json({ message: "Not Implemented" });
   } catch (err) {
     return next(err);
   }
 });
 
-router.get('/cashflow', async (req, res, next) => {
+router.get("/merchants", async (req, res, next) => {
   try {
-    res.status(501).json({ message: 'Not Implemented' });
-  } catch (err) {
-    return next(err);
-  }
-});
-
-router.get('/merchants', async (req, res, next) => {
-  try {
-    res.status(501).json({ message: 'Not Implemented' });
+    res.status(501).json({ message: "Not Implemented" });
   } catch (err) {
     return next(err);
   }
